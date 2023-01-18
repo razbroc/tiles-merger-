7fd2df7dac20cf9825b08568dbdb3394d03ea007
BROCHSTEIN RAZ <RAZBRO@rf.local>
2023-01-18 10:38:51 +0200

    complete(completed axisEdgeCoordinate test)

--- SQLiteDemo/ProgramTests/ProgramTests.cs
+++ SQLiteDemo/ProgramTests/ProgramTests.cs
@@ -1,15 +1,13 @@
 using DbTests;
 using Moq;
 using SQLiteDemo;
-using System.Data;
-using System.Data.Common;
 using System.Data.SQLite;
 
 namespace ProgramTests
 {
     [TestClass]
     public class DBTests
-    {/*
+    {
         #region consts
 
         const int X_MIN_COL = 5;
@@ -18,7 +16,7 @@ namespace ProgramTests
         const int Y_MAX_COL = 8;
 
         #endregion
-        */
+
 
         #region mocks
 
@@ -39,10 +37,10 @@ namespace ProgramTests
 
 
         [TestMethod]
-        public void AxisEdgeCoordinategffgs()
+        public void AxisEdgeCoordinategFindingMaxWhenMixedInMultipleRows()
         {
             // ARRANGE
-            SQLiteDataReaderMock= new Mock<DataReaderWrapper>(MockBehavior.Loose);
+            SQLiteDataReaderMock = new Mock<DataReaderWrapper>(MockBehavior.Loose);
             string path = InMemoryDBTemplate.GetGpkgPath();
             SQLiteConnection fakeConnection = new SQLiteConnection($"Data Source={path}");
             fakeConnection.Open();
@@ -51,14 +49,21 @@ namespace ProgramTests
                                           $"('test',4326,-180,-90,180,90);";
             SQLiteCommand command = new SQLiteCommand(query, fakeConnection);
             command.ExecuteNonQuery();
+            query = "INSERT INTO \"gpkg_contents\" (\"table_name\",\"data_type\",\"min_x\",\"min_y\",\"max_x\",\"max_y\") " +
+                                              "VALUES ('test','tiles',34.2663935002085,31.1786148130457,31.3258795317408,33.23180570002);" +
+                                              "VALUES ('test','tiles',34.2663935002085,31.1786148130457,34.3258795317408,31.23180570002);" +
+                                              "VALUES ('test','tiles',34.2663935002085,31.1786148130457,32.3258795317408,32.23180570002);";
+            command = new SQLiteCommand(query, fakeConnection);
+            command.ExecuteNonQuery();
+            Coordinate Expected = new Coordinate(34.3258795317408, 33.23180570002);
             //SQLiteDataReaderMock.SetupSequence(x => x.Read()).Returns(true).Returns(true).Returns(false);
             //SQLiteDataReaderMock.SetupSequence(x => x.GetDouble(It.IsAny<int>())).Returns(new Queue<double>(new[] { 1.35, 1.123, 34.3258795317408, 31.23180570002 }).Dequeue);
 
             // ACT
-            //Coordinate actualCoordinate = DbQueries.AxisEdgeCoordinates("stub", Math.Max, 0, 0, stubConn);
+            Coordinate actualCoordinate = DbQueries.AxisEdgeCoordinates(path, Math.Max, X_MAX_COL, Y_MAX_COL, fakeConnection);
 
             // ASSERT
-            //Assert.AreEqual(actualCoordinate, Expected);
+            Assert.AreEqual(actualCoordinate, Expected);
         }
     }
 }
