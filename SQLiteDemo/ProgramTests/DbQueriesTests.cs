cc5ea3d16764591aef63e9afc75fb328459f93f0
BROCHSTEIN RAZ <RAZBRO@rf.local>
2023-01-19 17:57:51 +0200

    update(used dynamic data in tests om imdb)

--- SQLiteDemo/ProgramTests/DbQueriesTests.cs
+++ SQLiteDemo/ProgramTests/DbQueriesTests.cs
@@ -16,8 +16,27 @@ namespace DbQueriesTests
         const int Y_MAX_COL = 8;
 
         #endregion
+        public static IEnumerable<object[]> AdditionData
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] { 
+                        new List<TileContent> {
+                            new TileContent("test", "tiles", 34.2663935002085, 31.1786148130457, 31.3258795317408, 33.23180570002),
+                            new TileContent("test1", "tiles", 34.2663935002085, 31.1786148130457, 34.3258795317408, 31.23180570002),
+                            new TileContent("test2", "tiles", 34.2663935002085, 31.1786148130457, 32.3258795317408, 32.23180570002)
+                        },
+                        new Coordinate(31.2663935002085, 31.1786148130457), X_MAX_COL, Y_MAX_COL
+                    }
+                };
+            }
+        }
 
-        public void TestAxisEdgeCoordinate(List<TileContent> contentToAdd, Coordinate expected, int xCol, int yCol, Coordinate.CompareFunc compareValue)
+        [TestMethod]
+        [DynamicData(nameof(AdditionData))]
+        public void TestAxisEdgeCoordinateMax(List<TileContent> contentToAdd, Coordinate expected, int xCol, int yCol)
         {
             // ARRANGE
             string path = InMemoryDBTemplate.GetGpkgPath();
@@ -28,40 +47,60 @@ namespace DbQueriesTests
             InMemoryDBTemplate.InsertIntoGpkgContents(fakeConnection, contentToAdd);
 
             // ACT
-            Coordinate actualCoordinate = DbQueries.AxisEdgeCoordinates(compareValue, xCol, yCol, fakeConnection);
+            Coordinate actualCoordinate = DbQueries.AxisEdgeCoordinates(Math.Max, xCol, yCol, fakeConnection);
 
             // ASSERT
             Assert.AreEqual(actualCoordinate, expected);
         }
 
         [TestMethod]
-        public void AxisEdgeCoordinategFindingMaxWhenMixedInMultipleRows()
+        [DynamicData(nameof(AdditionData))]
+        public void TestAxisEdgeCoordinateMin(List<TileContent> contentToAdd, Coordinate expected, int xCol, int yCol)
         {
             // ARRANGE
-            List<TileContent> contentToAdd = new List<TileContent>();
-            contentToAdd.Add(new TileContent("test", "tiles", 34.2663935002085, 31.1786148130457, 31.3258795317408, 33.23180570002));
-            contentToAdd.Add(new TileContent("test1", "tiles", 34.2663935002085, 31.1786148130457, 34.3258795317408, 31.23180570002));
-            contentToAdd.Add(new TileContent("test2", "tiles", 34.2663935002085, 31.1786148130457, 32.3258795317408, 32.23180570002));
-            Coordinate expected = new Coordinate(34.3258795317408, 33.23180570002);
+            string path = InMemoryDBTemplate.GetGpkgPath();
+            SQLiteConnection fakeConnection = new SQLiteConnection($"Data Source={path}");
+            fakeConnection.Open();
+            InMemoryDBTemplate.Create(path);
+            InMemoryDBTemplate.InsertDefaultIntoTileMatrix(fakeConnection);
+            InMemoryDBTemplate.InsertIntoGpkgContents(fakeConnection, contentToAdd);
 
-            // Act and assert
-            TestAxisEdgeCoordinate(contentToAdd, expected, X_MAX_COL, Y_MAX_COL, Math.Max);
+            // ACT
+            Coordinate actualCoordinate = DbQueries.AxisEdgeCoordinates(Math.Min, xCol, yCol, fakeConnection);
+
+            // ASSERT
+            Assert.AreEqual(actualCoordinate, expected);
         }
 
+        //[TestMethod]
+        //[DynamicData(nameof(AdditionData))]
+        //public void AxisEdgeCoordinategFindingMaxWhenMixedInMultipleRows()
+        //{
+        //    // ARRANGE
+        //    List<TileContent> contentToAdd = new List<TileContent>();
+        //    contentToAdd.Add(new TileContent("test", "tiles", 34.2663935002085, 31.1786148130457, 31.3258795317408, 33.23180570002));
+        //    contentToAdd.Add(new TileContent("test1", "tiles", 34.2663935002085, 31.1786148130457, 34.3258795317408, 31.23180570002));
+        //    contentToAdd.Add(new TileContent("test2", "tiles", 34.2663935002085, 31.1786148130457, 32.3258795317408, 32.23180570002));
+        //    Coordinate expected = new Coordinate(34.3258795317408, 33.23180570002);
+
+        //    // Act and assert
+        //    TestAxisEdgeCoordinate(contentToAdd, expected, X_MAX_COL, Y_MAX_COL, Math.Max);
+        //}
 
 
-        [TestMethod]
-        public void AxisEdgeCoordinategFindingMinWhenMixedInMultipleRows()
-        {
-            // ARRANGE
-            List<TileContent> contentToAdd = new List<TileContent>();
-            contentToAdd.Add(new TileContent("test", "tiles", 31.2663935002085, 32.1786148130457, 31.3258795317408, 33.23180570002));
-            contentToAdd.Add(new TileContent("test1", "tiles", 32.2663935002085, 31.1786148130457, 34.3258795317408, 31.23180570002));
-            contentToAdd.Add(new TileContent("test2", "tiles", 34.2663935002085, 33.1786148130457, 32.3258795317408, 32.23180570002));
-            Coordinate expected = new Coordinate(31.2663935002085, 31.1786148130457);
 
-            // Act and assert
-            TestAxisEdgeCoordinate(contentToAdd, expected, X_MIN_COL, Y_MIN_COL, Math.Min);
-        }
+        //[TestMethod]
+        //public void AxisEdgeCoordinategFindingMinWhenMixedInMultipleRows()
+        //{
+        //    // ARRANGE
+        //    List<TileContent> contentToAdd = new List<TileContent>();
+        //    contentToAdd.Add(new TileContent("test", "tiles", 31.2663935002085, 32.1786148130457, 31.3258795317408, 33.23180570002));
+        //    contentToAdd.Add(new TileContent("test1", "tiles", 32.2663935002085, 31.1786148130457, 34.3258795317408, 31.23180570002));
+        //    contentToAdd.Add(new TileContent("test2", "tiles", 34.2663935002085, 33.1786148130457, 32.3258795317408, 32.23180570002));
+        //    Coordinate expected = new Coordinate(31.2663935002085, 31.1786148130457);
+
+        //    // Act and assert
+        //    TestAxisEdgeCoordinate(contentToAdd, expected, X_MIN_COL, Y_MIN_COL, Math.Min);
+        //}
     }
 }
